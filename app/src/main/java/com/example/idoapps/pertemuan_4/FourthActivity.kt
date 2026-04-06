475ca4af304fd4f0bb9411c22d4cc454da855d6b
lab156-26 <ido24si@mahasiswa.pcr.ac.id>
2026-04-06 08:01:46 +0700

    Pengiriman data Intent

---
+++ app/src/main/java/com/example/idoapps/pertemuan_4/FourthActivity.kt
@@ -0,0 +1,29 @@
+package com.example.idoapps.pertemuan_4
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.idoapps.R
+import com.example.idoapps.databinding.ActivityThirdBinding
+
+class FourthActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_fourth)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val name = intent.getStringExtra("name")
+        val from = intent.getStringExtra("from")
+        val age = intent.getIntExtra("age",0)
+        Log.i("Data Intent","Nama: $name , Usia: $age, Asal: $from")
+    }
+}

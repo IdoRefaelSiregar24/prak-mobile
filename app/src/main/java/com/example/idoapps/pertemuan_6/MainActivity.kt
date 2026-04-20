e78b8243bb9c94691f820dd260d56880ea0863b7
lab156-26 <ido24si@mahasiswa.pcr.ac.id>
2026-04-20 09:45:56 +0700

    Pertemuan 6: Fitur SharedPreferences

--- app/src/main/java/com/example/idoapps/pertemuan_6/MainActivity.kt
+++ app/src/main/java/com/example/idoapps/pertemuan_6/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.idoapps.pertemuan_6
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
@@ -16,6 +17,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMain2Binding
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMain2Binding.inflate(layoutInflater)
@@ -28,13 +30,20 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+        val sharedPref = getSharedPreferences("user_pref", MODE_PRIVATE)
+
         binding.btnLogout.setOnClickListener {
             MaterialAlertDialogBuilder(this)
                 .setTitle("Konfirmasi")
                 .setMessage("Apakah Anda yakin ingin logout?")
                 .setPositiveButton("Ya") { dialog, _ ->
                     dialog.dismiss()
-                    Log.e("Info Dialog","Anda memilih Ya!")
+                    val editor = sharedPref.edit()
+                    editor.clear()
+                    editor.apply()
+
+                    val intent = Intent(this, AuthActivity::class.java)
+                    startActivity(intent)
                     finish()
                 }
                 .setNegativeButton("Batal") { dialog, _ ->
